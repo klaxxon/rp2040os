72df50c48c6d134cf53dfc314e621b756abab77e
Jonathan Gettys <jgettys@novanetic.com>
2021-02-13 18:34:13 -0600

    Fixes for multi-core support

--- main.c
+++ main.c
@@ -32,40 +32,42 @@
 #define clrGPIO(x) (*(uint32_t*)(SIO_BASE+GPIO_OUT_CLR) = (1<<x))
 
 typedef struct CpuStats {
-  uint64_t contextTime, idleTime;
+  uint64_t contextTime;
+  uint64_t idleTime;
+  uint64_t lastContextTime;
   uint32_t ticks;
 } CpuStats;
 
-uint64_t wallTime;
 CpuStats cpuStats[MAX_CORES];
 
 
 enum threadStatus {
   THREAD_STATUS_IDLE = 0,
-  THREAD_STATUS_RUNNING = 1,
+  THREAD_STATUS_RUNNING = 1,// Can be run
   THREAD_STATUS_WAIT = 2,
   THREAD_STATUS_DONE = 3,
 };
 
 typedef struct Thread {
   uint32_t stackPtr;  // Top of stack
+  uint8_t pid;
   volatile enum threadStatus status;
   uint64_t execTime;
-  uint64_t waitExpires;
+  volatile uint64_t waitExpires;
   #ifdef USE_THREAD_NAMES
   char *name;
   #endif
   uint8_t priority;
-  uint8_t rrtag; // Used for round-robin tracking
+  volatile uint8_t cpu; // Active on CPU, 255 = not active
+  volatile uint8_t lastcpu; // Last cpu used
 } Thread;
 
 
 struct Thread threads[MAX_TASKS];
 volatile uint8_t threadCount = MAX_CORES;  // 0-MAX_CORES is idleThread
-volatile uint8_t currentThreadIdx;
 volatile uint8_t currentIdx = 0;
 // These are so the assembly does not need to use index
-Thread *currentThread;
+Thread *currentThread[MAX_CORES];
 //Thread *nextThread;
 
 static volatile uint32_t sysCount, idleCount;
@@ -76,44 +78,68 @@ void startStackThread(uint32_t stackPtr);
 void setPSPControl(uint32_t sp, uint32_t ctrl);
 
 
-void isr_systick(void) {
-  uint8_t cpu = *(uint32_t*)(SIO_BASE);
-  cpuStats[cpu].ticks++;
-  // Kick off PendSV
-  if (!cpu)*(volatile uint32_t *)(0xe0000000|M0PLUS_ICSR_OFFSET) = (1L<<28);
+uint64_t getTimeUs() {
+  uint64_t tm = *(uint32_t*)(TIMER_BASE + 0x0c);
+  tm |= (*(uint32_t*)(TIMER_BASE + 0x08))<<16;
+  return tm;
 }
 
 static inline void contextLock() {
-  while (!*CONTEXTSWITCH_SPINLOCK);
+  uint8_t cpu = *(uint32_t*)(SIO_BASE);
+  while (!*CONTEXTSWITCH_SPINLOCK) {
+    uint32_t z = *(uint32_t*)(SIO_BASE+0x5c);
+    z++;
+  }
 }
 
 static inline void contextUnlock() {
   *CONTEXTSWITCH_SPINLOCK = 0;
 }
+/*
+uint16_t logs[32]= {0};
+uint8_t logpos = 0;
+
+void addLog(uint16_t cpu, uint16_t thrd,uint16_t log) {
+  while (!*((uint32_t*)(SIO_BASE+SIO_SPINLOCK1_OFFSET)));
+  logs[logpos++] = (cpu*1000)+(thrd*100)+log;
+  logpos &=31;
+  *((uint32_t*)(SIO_BASE+SIO_SPINLOCK1_OFFSET)) = 1;
+}
+*/
+
+void isr_systick(void) {
+  uint8_t cpu = *(uint32_t*)(SIO_BASE);
+  cpuStats[cpu].ticks++;
+  // Kick off PendSV
+  *(volatile uint32_t *)(0xe0000000|M0PLUS_ICSR_OFFSET) = (1L<<28);
+}
 
 void contextSwitch() {
-  uint32_t now = SYSTICKVAL;
+  uint32_t now = getTimeUs();
   uint8_t cpu = *(uint32_t*)(SIO_BASE);
-  uint32_t elapsed = (SYSTCOUNTER - now);
-  if ((*(volatile uint32_t *)(0xe0000000|M0PLUS_SYST_CSR_OFFSET))&0x10000) {
-    // Counter reset
-    elapsed += SYSTCOUNTER;
-  }
-  if (!cpu) wallTime += elapsed;
+  uint32_t elapsed = (now - cpuStats[cpu].lastContextTime);
+  cpuStats[cpu].lastContextTime = now;
 
-  setGPIO(OSC_PIN);
+  setGPIO(SQ_PIN+cpu);
   contextLock();
-  //currentThread = &threads[currentIdx];
-  currentThread->execTime += elapsed;
+  currentThread[cpu]->execTime += elapsed;
+  currentThread[cpu]->lastcpu = cpu;
+  currentThread[cpu]->cpu = 0xff;
   sysCount++;
+  if (cpu == 1) {
+    cpu = 1;
+  }
 
   uint8_t cpriority = 255;
   // Unblock anything waiting if time is up
   // Find highest priority
   for(uint8_t a=MAX_CORES;a<threadCount;a++) {
     Thread *t = &threads[a];
+    // Ignore if active on another core
+
+    if (t->cpu != 0xff && t->cpu != cpu) continue;
     if (t->status == THREAD_STATUS_WAIT) {
-      if (t->waitExpires <= wallTime) {
+      if (t->waitExpires <= now) {
         t->status = THREAD_STATUS_RUNNING;
       }
     }
@@ -123,12 +149,14 @@ void contextSwitch() {
   }
 
   // Now find processes matching highest priority
-  if (currentIdx < MAX_CORES) currentIdx = MAX_CORES; // Skip idle threads
+  if (currentIdx < MAX_CORES) currentIdx = MAX_CORES-1; // Skip idle threads
   uint8_t a;
   for(a=MAX_CORES;a<threadCount;a++) {
     currentIdx++;
     if (currentIdx >= threadCount) currentIdx = MAX_CORES;
     Thread *t = &threads[currentIdx];
+    // Ignpore if active on another cpu
+    if (t->cpu != 0xff && t->cpu != cpu) continue;
     // Ignore if not running
     if (t->status != THREAD_STATUS_RUNNING) continue;
 
@@ -136,66 +164,68 @@ void contextSwitch() {
     if (t->priority != cpriority) continue;
     break;
   }
-  if (a >= threadCount) currentIdx = cpu; // Idle on appropriate core
-  currentThread = &threads[currentIdx];
+  if (a >= threadCount) {
+    currentIdx = cpu; // Idle on appropriate core
+  }
+  currentThread[cpu] = &threads[currentIdx];
+  currentThread[cpu]->cpu = cpu;
   contextUnlock();
-  clrGPIO(OSC_PIN);
-  uint32_t ctime = now - SYSTICKVAL; // Context time
+  clrGPIO(SQ_PIN+cpu);
+  uint32_t ctime = getTimeUs()-now; // Context time
   cpuStats[cpu].contextTime +=  ctime; // SysTick counter decrements
-  if (!cpu) wallTime += ctime; // Time in context
 }
 
 static uint32_t idleStack[MAX_CORES][IDLE_STACKSIZE];
 void idle() {
-  while (true); asm("wfi"); 
+  while (true) asm("wfi"); 
+}
+
+
+void setupIdle(uint8_t cpu) {
+  Thread *t = &threads[cpu];
+  t->execTime = 0;
+  t->status = THREAD_STATUS_RUNNING;
+  t->priority = 255;
+  t->cpu = 0xff; // Not active
+  t->pid = cpu;
+  #ifdef USE_THREAD_NAMES
+  t->name = "IdleThread";
+  #endif
+  // Stack builds top down
+  uint8_t len = IDLE_STACKSIZE;
+  idleStack[cpu][--len] = 0x01000000; // Thumb bit of xPSR
+  idleStack[cpu][--len] = (uint32_t)idle;  // start of thread routine
+  t->stackPtr = (uint32_t)(idleStack[cpu] + IDLE_STACKSIZE - 16);
+  currentThread[cpu] = t;
+  setPSPControl(t->stackPtr, 2); // Privileged
 }
 
 
 void core1_main() {
+  setupIdle(1);
   // Systick
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SYST_RVR_OFFSET) = (SYSTEM_CLOCK_HZ / 1000000)*SCHED_INTERVAL_US; // reload count
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SYST_CSR_OFFSET) = 7; // enable, SYSTICK at core clock, exception
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SHPR3_OFFSET) = (0<<30) | (3<<22); // SysTick=0(high), PendSV=3(low) 
-  gpio_init(17);
-  gpio_set_dir(17, GPIO_OUT);
-  while (1) {
-    setGPIO(17);
-    clrGPIO(17);
-  }
+  startStackThread(currentThread[1]->stackPtr); // Does not return
 }
-
-
 
 
 // Must supply pointer to first thread added
 void setupSched() {
-  // Idle threads
-  for(uint8_t a=0;a<MAX_CORES;a++) {
-    Thread *t = &threads[a];
-    t->execTime = 0;
-    t->status = THREAD_STATUS_RUNNING;
-    t->priority = 255;
-    #ifdef USE_THREAD_NAMES
-    t->name = "IdleThread";
-    #endif
-    // Stack builds top down
-    uint8_t len = IDLE_STACKSIZE;
-    idleStack[a][--len] = 0x01000000; // Thumb bit of xPSR
-    idleStack[a][--len] = (uint32_t)idle;  // start of thread routine
-    t->stackPtr = (uint32_t)(idleStack[a] + IDLE_STACKSIZE - 16);
-  }
+  for(uint8_t a=0;a<MAX_CORES;a++) cpuStats[a].lastContextTime = getTimeUs();
+  // Idle thread
+  setupIdle(0);
 
   // Starting thread
-  currentThread = &threads[0];
   currentIdx = MAX_CORES; // First user process
 
   // Systick
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SYST_RVR_OFFSET) = (SYSTEM_CLOCK_HZ / 1000000)*SCHED_INTERVAL_US; // reload count
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SYST_CSR_OFFSET) = 7; // enable, SYSTICK at core clock, exception
   *(volatile unsigned int *)(0xe0000000|M0PLUS_SHPR3_OFFSET) = (0<<30) | (3<<22); // SysTick=0(high), PendSV=3(low) 
-  setPSPControl(currentThread->stackPtr, 2); // Privileged
-  //multicore_launch_core1(core1_main);
-  startStackThread(currentThread->stackPtr);
+  multicore_launch_core1(core1_main);
+  startStackThread(currentThread[0]->stackPtr); // Does not return
   while(1);
 }
 
@@ -218,6 +248,8 @@ void addThread(void(*hndl)(void*), uint32_t *stack, uint16_t len) {
   t->execTime = 0;
   t->status = THREAD_STATUS_RUNNING;
   t->priority = priority;
+  t->cpu = 0xff; // Not active
+  t->pid = threadCount;
 #ifdef USE_THREAD_NAMES
   t->name = name;
 #endif
@@ -230,17 +262,14 @@ void addThread(void(*hndl)(void*), uint32_t *stack, uint16_t len) {
 }
 
 
-static inline void delay(uint32_t clks) {
+static inline void delayus(uint32_t us) {
   uint8_t threadID = currentIdx;
-  threads[threadID].waitExpires = wallTime + clks;
+  threads[threadID].waitExpires = getTimeUs() + us;
   threads[threadID].status = THREAD_STATUS_WAIT;
   yield();
-  // OS will change state when released
-  //while(threads[threadID].status == THREAD_STATUS_WAIT);
 }
 
-#define delayus(x) delay(x*(SYSTEM_CLOCK_HZ/1000000))
-#define delayms(x) delay(x*(SYSTEM_CLOCK_HZ/1000))
+#define delayms(x) delayus(x*1000)
 
 
 
@@ -270,18 +299,19 @@ void blink2() {
 
 static uint32_t repStack[256];
 void report() {
-  uint64_t lastTime = wallTime;
+  uint64_t lastTime = getTimeUs();
   uint64_t lasts[MAX_TASKS]={0};
   while (true) {
     delayms(1000);
-    uint64_t elapsed = wallTime - lastTime;
-    lastTime = wallTime;
-    printf("\nWall time %llu\n", wallTime/SYSTEM_CLOCK_HZ);
+    uint64_t now = getTimeUs();
+    uint64_t elapsed = now - lastTime;
+    lastTime = now;
+    printf("\nWall time %llu\n", now/1000000);
     for(uint8_t a=0;a<MAX_CORES;a++) {
-      printf("CPU%d Ctx=%0.3f%%, Idle=%0.3f%%\n", a, 100.0*cpuStats[a].contextTime/wallTime, 100.0*threads[a].execTime/wallTime);
+      printf("CPU%d Ctx=%0.3f%%, Idle=%0.3f%%\n", a, 100.0*cpuStats[a].contextTime/now, 100.0*threads[a].execTime/now);
     }
     #ifdef USE_THREAD_NAMES
-    printf("Thrd   Name     S PRI Total     CPU\n");
+    printf("Thrd   Name     S PRI  CPU     LastCPU\n");
     #else
     printf("Thrd   Total   S PRI CPU\n");
     #endif
@@ -302,10 +332,20 @@ void report() {
       } else printf("  ");
       uint64_t telapsed = t->execTime - lasts[a];
       lasts[a] = t->execTime;
-      printf("%3d %6.3f%%\n", t->priority, 100.0*telapsed/elapsed);
+      char cpu = '0' + t->lastcpu;
+      printf("%3d %6.3f%%  %c\n", t->priority, 100.0*telapsed/elapsed, cpu);
     }
+    /*
+    printf("Logs ");
+    for(uint8_t a=0;a<32;a++) {
+      printf("%04d ", logs[(logpos+a)&31]);
+    }
+    printf("\n");
+    */
   }
 }
+
+
 
 static uint32_t hogStack[128];
 void hog() {
@@ -316,6 +356,13 @@ void hog() {
     delayms(10);
   }
 }
+
+
+static uint32_t spinnerStack[128];
+void spinner() {
+  while (true);
+}
+
 
 int main() {
   stdio_init_all(); 
@@ -332,5 +379,6 @@ int main() {
   addThread("Green LED", blink2, blink2Stack, sizeof(blink2Stack)>>2, 100);
   addThread("Report", report, repStack, sizeof(repStack)>>2, 255);
   addThread("SquareHog", hog, hogStack, sizeof(hogStack)>>2, 0);
+  addThread("Spinner", spinner, spinnerStack, sizeof(spinnerStack)>>2, 150);
   setupSched(); // No return
 }
